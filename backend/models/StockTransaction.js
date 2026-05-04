6a31ba056feaec20beb109a141a2e8f8145ae2aa
kaaom3 <finkmata@gmail.com>
2026-05-04 09:59:14 +0700

    stork

--- backend/models/StockTransaction.js
+++ backend/models/StockTransaction.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-const transactionSchema = new mongoose.Schema({
+const stockTransactionSchema = new mongoose.Schema({
     itemType: { type: String, required: true },
     size: { type: String, required: true },
-    transactionType: { type: String, enum: ['IN', 'OUT', 'ADJUST', 'RETURN-USED', 'RETURN-DAMAGED'] },
+    transactionType: { 
+        type: String, 
+        required: true, 
+        // 💡 เพิ่ม 'OUT-USED' เข้าไปใน Enum (รายชื่อคำที่อนุญาตให้บันทึกได้)
+        enum: ['IN', 'OUT', 'ADJUST', 'RETURN-USED', 'RETURN-DAMAGED', 'OUT-USED'] 
+    },
     quantity: { type: Number, required: true },
     reason: { type: String },
     adminUser: { type: String, required: true }
 }, { timestamps: true });
 
-module.exports = mongoose.model('StockTransaction', transactionSchema);
-
+module.exports = mongoose.model('StockTransaction', stockTransactionSchema);
