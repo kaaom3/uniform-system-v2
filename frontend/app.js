c1d7ecc0b3ba92172de74d6c2ce98377c4fc1381
kaaom3 <finkmata@gmail.com>
2026-06-04 17:50:24 +0700

    Upครั้งใหญ่2

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:';
-const API_BASE_URL = isLocalhost ? 'http://localhost:3000' : 'https://uniform-system-hg0e.onrender.com';
+const API_BASE_URL = isLocalhost ? 'http://localhost:3000' : 'https://uniform-system-v2.onrender.com';
 
 const AppState = { currentUser: null };
 
